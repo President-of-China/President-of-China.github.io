36724aaf9a4537a19e44aeef6c62743213e60675
President-of-China <chinatelecom.me@gmail.com>
2022-09-01 19:58:32 +0800

    update

--- packer/dist/sw.js
+++ packer/dist/sw.js
@@ -1,6 +1,6 @@
 // These will be replaced at build-time by generate-service-worker-plugin.js
-const ASSETS = ["","js/download-project.620b0396a75a7f11be62.worker.js","assets/reset.80a6e1615fc013684ad8047dba5ce064.svg","assets/default-icon.290e09e569a1cab8e61ba93b0d23863f.png","js/vendors~icns~jszip~sha256.29796c3f138f5b09f547.js","js/icns.384a01703a6a0a6a9617.js","js/jszip.37fbbf375a8d1cbc7f91.js","js/p4.bdedd883e1a2ba255ec3.js","js/packager-options-ui.73ccce3da5aaf2a98678.js","js/sha256.f165e860a5146837e42d.js"];
-const CACHE_NAME = "p4-5198b44542d6ac68ce35a610bbcdf67a6a4bb2848f44b969ac1efa5355c3fe8d";
+const ASSETS = ["","js/download-project.620b0396a75a7f11be62.worker.js","js/p4.36d921a343fe8c9543c8.js"];
+const CACHE_NAME = "p4-23e5828ad6ee06c8ea9c51c3d8603ae8235e173f28df26e0b31c285ace9c4a11";
 const IS_PRODUCTION = true;
 
 const base = location.pathname.substr(0, location.pathname.indexOf('sw.js'));
