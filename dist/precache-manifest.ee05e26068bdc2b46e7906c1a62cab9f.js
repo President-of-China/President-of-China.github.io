8e87ca816017880871ea502349b5be31ab09ccd2
President-of-China <chinatelecom.me@gmail.com>
2022-09-03 17:49:56 +0800

    update

---
+++ dist/precache-manifest.ee05e26068bdc2b46e7906c1a62cab9f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "b32e9931976c99e4e956",
+    "url": "/css/app.4f4250f8.css"
+  },
+  {
+    "revision": "b32e9931976c99e4e956",
+    "url": "/js/app.285415a2.js"
+  },
+  {
+    "revision": "aedb603c2b819531d037",
+    "url": "/css/chunk-00ee8072.f0d361d9.css"
+  },
+  {
+    "revision": "aedb603c2b819531d037",
+    "url": "/js/chunk-00ee8072.e3df78ac.js"
+  },
+  {
+    "revision": "378afa1dbce80c839086",
+    "url": "/js/chunk-0b046b52.d45ef4ab.js"
+  },
+  {
+    "revision": "82e5271ff7f63783f760",
+    "url": "/css/chunk-22239e24.f0d361d9.css"
+  },
+  {
+    "revision": "82e5271ff7f63783f760",
+    "url": "/js/chunk-22239e24.86af9af9.js"
+  },
+  {
+    "revision": "1f2688bd1684060d9ca3",
+    "url": "/js/chunk-2d216257.df162628.js"
+  },
+  {
+    "revision": "1fd2a5dbf9d43aa9ab4a",
+    "url": "/js/chunk-2d238433.1735250e.js"
+  },
+  {
+    "revision": "81f848fd25028b92401a",
+    "url": "/css/chunk-2e73aee3.93c3732a.css"
+  },
+  {
+    "revision": "81f848fd25028b92401a",
+    "url": "/js/chunk-2e73aee3.664b36aa.js"
+  },
+  {
+    "revision": "85b0a0409457d879d30a",
+    "url": "/css/chunk-34d2c1c4.e2e10d2a.css"
+  },
+  {
+    "revision": "85b0a0409457d879d30a",
+    "url": "/js/chunk-34d2c1c4.90e8f62f.js"
+  },
+  {
+    "revision": "b332b650dff31e215d4a",
+    "url": "/css/chunk-5d3ae0d8.f0d361d9.css"
+  },
+  {
+    "revision": "b332b650dff31e215d4a",
+    "url": "/js/chunk-5d3ae0d8.c0534173.js"
+  },
+  {
+    "revision": "4858e9b24884bdbc95ee",
+    "url": "/js/chunk-a4189212.a6657841.js"
+  },
+  {
+    "revision": "b3bfbdecc0b444d0cec1",
+    "url": "/css/chunk-a4b3ff6c.864c088c.css"
+  },
+  {
+    "revision": "b3bfbdecc0b444d0cec1",
+    "url": "/js/chunk-a4b3ff6c.566ba83b.js"
+  },
+  {
+    "revision": "d472166369a24cef85c0",
+    "url": "/js/element.7a4800b8.js"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "cd382dc8a9d6317864b5810a320effc5",
+    "url": "/fonts/league-gothic.cd382dc8.woff"
+  },
+  {
+    "revision": "9900a4643cc63c5d8f969d2196f72572",
+    "url": "/fonts/league-gothic.9900a464.eot"
+  },
+  {
+    "revision": "91295fa87df918411b49b7531da5d558",
+    "url": "/fonts/league-gothic.91295fa8.ttf"
+  },
+  {
+    "revision": "d9132906145c5df3af3fdfd3dc002ff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf35c3805e83f0178f9e224d8d0febca",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "369f6129748a531567ab3535689be69a",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
